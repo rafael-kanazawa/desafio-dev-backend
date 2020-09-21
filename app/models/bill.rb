24ee50c5df50357b49ce4ce0c4a1becab10ca461
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2020-09-21 21:28:02 +0000

    Adding validations

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,5 +2,6 @@ class Bill < ApplicationRecord
     has_many :orders, dependent: :destroy
     belongs_to :table
 
-
+    validates :status, numericality: {only_integer: true}, presence: true
+    validates :amount, numericality: true, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,7 @@
 class Order < ApplicationRecord
     belongs_to :bill
+    belongs_to :menu_item
+
+    validates :status, :quantity, :bill_id, numericality: {only_integer: true}, presence: true
+    validates :note, presence: true
 end
