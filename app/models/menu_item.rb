a8eac2cfbdaf8fce3ae70bb3c4251e383b4bf88b
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2020-09-21 20:19:59 +0000

    Adding validations

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,3 +1,6 @@
 class MenuItem < ApplicationRecord
     belongs_to :category
+
+    validates :price, numericality: true, presence: true
+    validates :dish_name, :category_id, :description , presence: true
 end
