88436856eca6a96e71e33e7011e5ebfda405f6eb
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2020-09-21 20:46:47 +0000

    Testing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :menu_items
-  resources :categories
+  resources :menu_items, only: [:index, :create, :update, :destroy]
+  resources :categories, only: [:index, :create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
