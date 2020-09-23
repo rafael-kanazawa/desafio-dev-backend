114db13750f24821622c549b2484c6be23fe7ce7
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2020-09-23 06:11:05 +0000

    Adding custom method and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   resources :tables
   resources :bills do
     resources :orders
